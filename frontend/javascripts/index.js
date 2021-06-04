4c9bf523640b06de5ac3f55532058a49c319b501
Michael Martinez <zeroeclipse@live.com>
2021-06-03 18:02:57 -0700

    Working on show lists page.

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,28 +1,65 @@
-const listsButton = () => document.querySelector("#lists-button")
 const homeButton = () => document.querySelector("#home-button")
 
 const showLists = () => {
-    contentDiv().innerHTML = `
-        <header id="1"><br><br>
-            <h1 id="h1lists">My lists</h1><br><br>
-        </header>
-        <section id="list-list">
-        </section>
-        <br><br><br><br>
-        <div id="container">
-            <a id="button-anchor" href="/lists/new">
-                <div class="button1" id="button-3">
-                    <div id="circle"></div>
-                    <p id="button-text"> Create List</p>
-                </div>
-            </a>
+    document.querySelector('body').innerHTML = `
+
+    <div id="myNav" class="lists-overlay">
+    
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <div class="lists-overlay-content">
+        <div class="list-window">
+          <table class="styled-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+              <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Action</td>
+                <td>action animes</td>
+                <td> X</td>
+            </tr>
+          <tr>
+                <td>Demon Animes</td>
+                <td>like inuyasha</td>
+                <td> X</td>
+            </tr>
+          <tr>
+                <td>Cool stuff</td>
+                <td>my faves</td>
+            <td> X</td>
+            </tr>
+            <tr class="active-row">
+                <td>Romance </td>
+                <td>cute stuff</td>
+                <td> X</td>
+            </tr>
+            <!-- and so on... -->
+        </tbody>
+    </table>
+          <button>Create new list</button>
         </div>
+      </div>
+    
+    </div>
         `
 }
 
 const showHome = () => {
     contentDiv().innerHTML = `<img id="logo" src="./assets/animelistlogo6.png">`
 }
-listsButton().addEventListener("click", showLists)
+// listsButton().addEventListener("click", showLists)
 homeButton().addEventListener("click", showHome)
 
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  } 
