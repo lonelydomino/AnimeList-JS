0ad704fa5b902d778ad20e139188e87625fb8b21
Michael Martinez <zeroeclipse@live.com>
2021-06-11 16:51:35 -0700

    Fixing anime tables a bit.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # AnimeList-JS
+
+
+Things to do:
+-------------------
+[ ] - Add Registration functionality
+[ ] - Fix Lists styling
+[ ] - Fix List Animes styling
+[ ] - Fix search bar refreshing!!!
+[ ] - Move Search to backend
--- frontend/javascripts/services/AnimeApi.js
+++ frontend/javascripts/services/AnimeApi.js
@@ -71,12 +71,42 @@ static fetchAnimes(list_id) {
             </div>
 
             <h2 id="anime-table-name">${listName}</h2>
-            <ul class="responsive-table">
-                <li class="table-header">
-                    <div class="col col-1">Anime Name</div>
-                    <div class="col col-4"></div>
-                </li>
-            </ul>
+            <ul class="responsive-table" style="
+            left: 8%;
+            width: 90%;
+            position: absolute;
+        ">
+                  <li class="table-header">
+                    <div class="col col-1">Job Id</div>
+                    <div class="col col-2">Customer Name</div>
+                    <div class="col col-3">Amount Due</div>
+                    <div class="col col-4">Payment Status</div>
+                  </li>
+                  <li class="table-row">
+                    <div class="col col-1" data-label="Job Id">42235</div>
+                    <div class="col col-2" data-label="Customer Name">John Doe</div>
+                    <div class="col col-3" data-label="Amount">$350</div>
+                    <div class="col col-4" data-label="Payment Status">Pending</div>
+                  </li>
+                  <li class="table-row">
+                    <div class="col col-1" data-label="Job Id">42442</div>
+                    <div class="col col-2" data-label="Customer Name">Jennifer Smith</div>
+                    <div class="col col-3" data-label="Amount">$220</div>
+                    <div class="col col-4" data-label="Payment Status">Pending</div>
+                  </li>
+                  <li class="table-row">
+                    <div class="col col-1" data-label="Job Id">42257</div>
+                    <div class="col col-2" data-label="Customer Name">John Smith</div>
+                    <div class="col col-3" data-label="Amount">$341</div>
+                    <div class="col col-4" data-label="Payment Status">Pending</div>
+                  </li>
+                  <li class="table-row">
+                    <div class="col col-1" data-label="Job Id">42311</div>
+                    <div class="col col-2" data-label="Customer Name">John Carpenter</div>
+                    <div class="col col-3" data-label="Amount">$115</div>
+                    <div class="col col-4" data-label="Payment Status">Pending</div>
+                  </li>
+                </ul>
             `
       document.querySelector("aside").innerHTML += table
     }
